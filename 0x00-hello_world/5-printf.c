93b692e1c8aa91fb9eea592e1f835b35877b99e1
johnluka <jluka9421@gmail.com>
2022-12-09 09:30:59 -0800

    my print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome  art,\n");
+
+			return (0);
+
+}
