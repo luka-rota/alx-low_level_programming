f8e7be49bdae49ffac72d7a28065af2b41022b69
johnluka <jluka9421@gmail.com>
2022-12-09 10:17:17 -0800

    great

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* 
- * main: entry level
+/*
+ * Main - entry level
  *
- * return: always 0
+ * Return: Always 0 (success)
  */
 int main(void)
-
-	{
+{
 		puts("\"Programming is like building a multilingual puzzle");
+
 		return (0);
-	}
+}
